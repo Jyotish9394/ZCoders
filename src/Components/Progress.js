932e56643673b0bfac3016f674329de0fff523ec
Jyotish9394 <146106545+Jyotish9394@users.noreply.github.com>
2024-06-05 22:19:15 +0530

    Add src folders and files

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -8,39 +8,18 @@ import jsImage from './Images/js.png';
 export default function Navbar(props) {
     return (
         <>
-            <div className="line">Your Preparation</div>
+            
             <section className="second_section">
                 <div className="left_section_02">
-                    <div className="top_header">PREPARE BY TOPICS
-                        <img className="image_01" src={cppImage} alt="" />
-                    </div>
-                    <div className="header_1">C++</div>
-                    <button className="continue_preparation"><a href="a">Start Preparation</a></button>
+                    <div>Question:</div>
+                    <button className="continue_preparation"><a href="a">See More</a></button>
                 </div>
                 <div className="right_section_02">
-                    <div className="top_header">PREPARE BY TOPICS
-                        <img className="image_01" src={pythonImage} alt="" />
-                    </div>
-                    <div className="header_1">PYTHON</div>
-                    <button className="continue_preparation"><a href="a">Start Preparation</a></button>
+                    <div>Answer:</div>
+                    <button className="continue_preparation"><a href="a">See More</a></button>
                 </div>
             </section>
-            <section className="second_section">
-                <div className="left_section_02">
-                    <div className="top_header">PREPARE BY TOPICS
-                        <img className="image_01" src={javaImage} alt="" />
-                    </div>
-                    <div className="header_1">JAVA</div>
-                    <button className="continue_preparation"><a href="a">Start Preparation</a></button>
-                </div>
-                <div className="right_section_02">
-                    <div className="top_header">PREPARE BY TOPICS
-                        <img className="image_01" src={jsImage} alt="" />
-                    </div>
-                    <div className="header_1">JAVA SCRIPT</div>
-                    <button className="continue_preparation"><a href="a">Start Preparation</a></button>
-                </div>
-            </section>
+            
         </>
     )
 }
